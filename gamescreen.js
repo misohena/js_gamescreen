667c73f440b35a3cc08081f94cf63a71cd7cdebf
AKIYAMA Kouhei <misohena@gmail.com>
2014-10-17 12:02:06 +0900

    ウィンドウ内最大化時に高さが最大化されない場合がある不具合を解消した。
    ドキュメントの高さがウィンドウの高さより小さいときにドキュメントの高さま
    でしか最大化されなかった。
    window.innerHeightを使うようにした。window.innerHeightが使えないときは
    document.documentElement.clientHeightを使うようにした。

--- gamescreen.js
+++ gamescreen.js
@@ -89,8 +89,8 @@
         {
             document.body.style.overflow = "hidden"; //hide body scrollbar
 
-            var clientWidth = document.documentElement.clientWidth;
-            var clientHeight = document.documentElement.clientHeight;
+            var clientWidth = window.innerWidth || document.documentElement.clientWidth;
+            var clientHeight = window.innerHeight || document.documentElement.clientHeight;
 
             screenArea.style.display = "block";
             screenArea.style.position = "fixed";
